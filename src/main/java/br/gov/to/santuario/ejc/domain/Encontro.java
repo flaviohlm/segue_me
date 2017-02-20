6651cff2264abba623afb853906f46b6c4e59694
Flavio Henrich Leite Madureira <flavio.madureira@secad.local>
2017-02-20 15:02:35 -0300

    alteracoes de layout

--- src/main/java/br/gov/to/santuario/ejc/domain/Encontro.java
+++ src/main/java/br/gov/to/santuario/ejc/domain/Encontro.java
@@ -65,7 +65,7 @@ public class Encontro implements Serializable {
     private List<EncontroParoquia> encontroParoquiaList;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "encontro")
     private List<EncontroPalestra> encontroPalestraList;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "encontro")    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "encontro")        
     private List<EncontroEquipe> encontroEquipeList;
     @JoinColumn(name = "diretor_espiritual_id", referencedColumnName = "id")
     @ManyToOne(optional = false)
